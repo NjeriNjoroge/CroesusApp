866c8366889801ca0215e95485520f64ddba7bfe
NjeriNjoroge <grace.njoroge@incentro.com>
2020-04-21 14:03:02 +0300

    added a podfile

---
+++ CroesusApp/Support Files/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CroesusApp
+//
+//  Created by Grace Njoroge on 21/04/2020.
+//  Copyright © 2020 Grace. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    
+    window = UIWindow(frame: UIScreen.main.bounds)
+    window?.makeKeyAndVisible()
+    window?.rootViewController = UINavigationController(rootViewController: ViewController())
+    
+    return true
+  }
+
+}
+
---
+++ CroesusApp/Views/DataCollectionView.swift
@@ -0,0 +1,17 @@
+//
+//  DataCollectionView.swift
+//  CroesusApp
+//
+//  Created by Grace Njoroge on 21/04/2020.
+//  Copyright © 2020 Grace. All rights reserved.
+//
+
+import UIKit
+
+
+class DataCollectionView: UIView {
+  
+  
+
+
+}
